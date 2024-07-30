2927d5baee3437051d605d4c5ee44449c08f4c25
Edmore Moyo <etmoyo@gmail.com>
2024-07-30 17:56:41 -0400

    triggers error

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ dest = os.environ['OUTPUT_DIR']
 print("Command line arguments ...")
 print(sys.argv)
 print("ENV variables ...")
-print(os.environment)
+print(os.environ)
 
 shutil.copytree(src, dest, dirs_exist_ok=True)
 print("end of processing")
+sys.exit('Trigger error message')
