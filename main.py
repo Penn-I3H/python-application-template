62fb48e9bfe4518d1a5e626bf8ba139a92982cb3
Edmore Moyo <etmoyo@gmail.com>
2024-08-15 14:19:26 -0400

    test with try catch

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ import sys
 import shutil
 import os
 
-print("start of processing")
-src = os.environ['INPUT_DIR']
-dest = os.environ['OUTPUT_DIR']
 
-print("Command line arguments ...")
-print(sys.argv)
-print("ENV variables ...")
-print(os.environ)
 
-shutil.copytre(src, dest, dirs_exist_ok=True)
-print("end of processing")
-sys.exit(1)
+try:
+    print("start of processing")
+    src = os.environ['INPUT_DIR']
+    dest = os.environ['OUTPUT_DIR']
+
+    print("Command line arguments ...")
+    print(sys.argv)
+    print("ENV variables ...")
+    print(os.environ)
+    shutil.copytre(src, dest, dirs_exist_ok=True)
+    print("end of processing")
+except Exception as e:
+    sys.exit(e)
+
+
+
